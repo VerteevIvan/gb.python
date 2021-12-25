9d953843be3f1a0fdda7683e0af0d6164d8a39d3
Ivan Verteev <vanek26rus2009@bk.ru>
2021-12-25 14:34:50 +0300

    task_5_3

--- lesson_5/task_5_1.py
+++ lesson_5/task_5_1.py
@@ -1,18 +1,25 @@
-'''Написать генератор нечётных чисел от 1 до n (включительно), используя ключевое слово yield, например:
->>> odd_to_15 = odd_nums(15)
->>> next(odd_to_15)
-1
->>> next(odd_to_15)
-3
-...
->>> next(odd_to_15)
-15
->>> next(odd_to_15)
-...StopIteration...
+'''Написать генератор нечётных чисел от 1 до n (включительно),
+используя ключевое слово yield, например:
 '''
 def odd_nums(end):
     for i in range(1, end+1, 2):
         yield i
 
 odd_to_15 = odd_nums(15)
-next(odd_to_15)
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+#print(next(odd_to_15))
+
+"""
+*(вместо 1) Решить задачу генерации нечётных чисел от 1 до n (включительно), 
+не используя ключевое слово yield."""
+
+max_val = 3
+odd_nums_gen = (n for n in range(1, max_val + 1, 2))
+print(next(odd_nums_gen))
